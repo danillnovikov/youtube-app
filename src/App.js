6e6ff645227c6eca23a1ae865f7248d4c2861723
Daniil <danill.novikov@mail.ru>
2022-09-27 20:33:57 +0300

    add registration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Registration from './components/Registration/Registration';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </div>
---
+++ src/components/Registration/Registration.jsx
@@ -0,0 +1,67 @@
+import { Form, Input, Button } from 'antd';
+import axios from 'axios';
+import { message } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+const Registration = () => {
+  const navigate = useNavigate();
+
+  const onFinish = (values) => {
+    axios
+      .post('https://typ-back-end.herokuapp.com/api/users/register', {
+        login: values.email,
+        password: values.password,
+        isAdmin: false,
+      })
+      .then((response) => {
+        console.log(response);
+        if (+response.request.status === 200)
+          message.success('Зарегистрирован');
+        navigate('/login');
+      })
+      .catch(() => message.error('Ошибка регистрации'));
+  };
+
+  return (
+    <div className="home">
+      <h1 className="home_title">YouTube App</h1>
+      <Form onFinish={onFinish}>
+        <label className="lable" htmlFor="username">
+          Login
+        </label>
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your email!',
+            },
+          ]}
+        >
+          <Input id="email" placeholder="email" autoComplete="off"></Input>
+        </Form.Item>
+        <label className="lable" htmlFor="password">
+          Password
+        </label>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password id="password" placeholder="password"></Input.Password>
+        </Form.Item>
+        <Form.Item>
+          <Button className="btn" type="primary" htmlType="submit">
+            Registration
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Registration;
